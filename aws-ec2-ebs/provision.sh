61991ec78f04c768265122a3ede2bbb9416ed985
Shakeeljaveed <shakeeljaveed2k12@gmail.com>
2018-07-17 19:02:23 +0530

    Packer AWS-AMI json files and template

---
+++ aws-ec2-ebs/provision.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
---
+++ aws-ec2-instance/provision.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+
